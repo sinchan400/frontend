5caa0307b9c01f0ad0fc919a9464ff1f902b115c
sinchan400 <sinchananaik389@gmail.com>
2023-07-10 15:51:10 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./Components/Home";
+import Menu from "./Components/Menu";
+import About from "./Components/About";
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Navbar from "./Components/Navbar";
+function App(){
+  return(
+  <>
+  <BrowserRouter>
+  <Navbar/>
+  <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/menu" element={<Menu/>}/>
+    <Route path="/about" element={<About/>}/>
+  </Routes>
+  </BrowserRouter>
+  </>
   );
 }
-
 export default App;
